ba97c24e338c1b1873f79315df0a53a1d42f97e1
ZeyadOsama <zeyad.osama@gmail.com>
2019-11-16 00:32:47 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+///
+/// main.c
+/// Semaphore
+///
+/// @author Zeyad Osama.
+/// @date 2019-11-16.
+/// @copyright © 2019 Zeyad Osama. All rights reserved.
+///
+
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+int main() {
+    return 0;
+} 
+
